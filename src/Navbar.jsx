f8711d7262d0a30fa43bf88434ca9138715bd780
George <gmeadors@alphaworks.tech>
2024-04-16 01:55:58 -0700

    added log to navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,15 @@
-
-export default function Navbbar() {
+import logo from "./images/logo.png"
+export default function Navbar() {
     return (
-        <navbar className="navbar">
-          
+        <nav className="navbar">
+            <img src={logo} alt="logo"/>
+                      
         <ul> 
             <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
             <li><a href="#">Services</a></li>
             <li><a href="#">Contact</a></li>
         </ul>
-    </navbar>
+    </nav>
     );
 }
